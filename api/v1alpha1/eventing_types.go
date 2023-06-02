c86aeb0cb1471bbdef222d1585baf28a734a6323
Muhammad Faizan <m.faizan@sap.com>
2023-06-02 10:45:30 +0200

    added Eventing CRD

--- api/v1alpha1/eventing_types.go
+++ api/v1alpha1/eventing_types.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,17 +27,30 @@ import (
 
 // EventingSpec defines the desired state of Eventing
 type EventingSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Backends defines the list of eventing backends to provision.
+	Backends []Backend `json:"backends"`
 
-	// Foo is an example field of Eventing. Edit eventing_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Publisher defines the configurations for eventing-publisher-proxy.
+	// +optional
+	Publisher `json:"publisher,omitempty"`
+
+	// Logging defines the log level for eventing-manager.
+	// +optional
+	Logging `json:"logging,omitempty"`
+
+	// Annotations allows to add annotations to resources.
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// Labels allows to add Labels to resources.
+	// +optional
+	Labels map[string]string `json:"labels,omitempty"`
 }
 
 // EventingStatus defines the observed state of Eventing
 type EventingStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	State      string             `json:"state"`
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -57,6 +72,78 @@ type EventingList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []Eventing `json:"items"`
+}
+
+// Backend defines eventing backend.
+type Backend struct {
+	// Type defines which backend to use. The value is either `EventMesh`, or `NATS`.
+	// +kubebuilder:validation:Enum=EventMesh;NATS
+	Type string `json:"type"`
+
+	// Config defines configuration for eventing backend.
+	// +optional
+	Config BackendConfig `json:"config"`
+}
+
+// BackendConfig defines configuration for eventing backend.
+type BackendConfig struct {
+	// NatsStorageType defines the storage type for stream data.
+	// +optional
+	// +kubebuilder:validation:Enum=File;Memory
+	NATSStorageType string `json:"natsStorageType"`
+
+	// NatsStorageSize defines the storage size for stream data.
+	// +optional
+	NATSStorageSize resource.Quantity `json:"natsStorageSize"`
+
+	// NatsStreamReplicas defines the number of replicas for stream.
+	// +optional
+	NATSStreamReplicas int `json:"natsStreamReplicas"`
+
+	// MaxStreamSize defines the maximum storage size for stream data.
+	// +optional
+	MaxStreamSize resource.Quantity `json:"maxStreamSize"`
+
+	// MaxMsgsPerTopic limits how many messages in the NATS stream to retain per subject.
+	// +optional
+	MaxMsgsPerTopic int `json:"maxMsgsPerTopic"`
+
+	// EventMeshSecret defines the namespaced name of K8s Secret containing EventMesh credentials. The format of name is "namespace/name".
+	// +optional
+	EventMeshSecret string `json:"eventMeshSecret"`
+
+	// WebhookAuthSecret defines the namespaced name of K8s Secret containing Webhook auth credentials. The format of name is "namespace/name".
+	// +optional
+	WebhookAuthSecret string `json:"webhookAuthSecret"`
+}
+
+// Publisher defines the configurations for eventing-publisher-proxy.
+type Publisher struct {
+	// Replicas defines the scaling min/max for eventing-publisher-proxy.
+	// +optional
+	Replicas `json:"replicas,omitempty"`
+
+	// Resources defines resources for eventing-publisher-proxy.
+	// +optional
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+}
+
+// Replicas defines min/max replicas for a resource.
+type Replicas struct {
+	// Min defines minimum number of replicas.
+	// +optional
+	Min int `json:"min,omitempty"`
+
+	// Max defines maximum number of replicas.
+	// +optional
+	Max int `json:"max,omitempty"`
+}
+
+type Logging struct {
+	// LogLevel defines the log level.
+	// +optional
+	// +kubebuilder:validation:Enum=Info;Warn;Error;debug
+	LogLevel string `json:"logLevel,omitempty"`
 }
 
 func init() {
