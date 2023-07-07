8e6fb84e19d0a099c67c79eb45dcf5e5264a000b
Carina Kothe <carina.kothe@sap.com>
2023-07-07 17:49:01 +0200

    First validation integration testcases

---
+++ internal/controller/eventing/integrationtests/validation/integration_test.go
@@ -0,0 +1,271 @@
+package validation_test
+
+import (
+	"github.com/kyma-project/eventing-manager/testutils"
+	"github.com/kyma-project/eventing-manager/testutils/integration"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"os"
+	"testing"
+)
+
+const projectRootDir = "../../../../../"
+
+const noError = ""
+
+const (
+	kind               = "kind"
+	kindEventing       = "Eventing"
+	apiVersion         = "apiVersion"
+	apiVersionEventing = "operator.kyma-project.io/v1alpha1"
+	metadata           = "metadata"
+	name               = "name"
+	namespace          = "namespace"
+
+	spec                  = "spec"
+	backends              = "backends"
+	backendType           = "type"
+	typeEventMesh         = "EventMesh"
+	typeNats              = "NATS"
+	typeGibberish         = "Gibberish"
+	config                = "config"
+	natsStreamStorageType = "natsStreamStorageType"
+	storageTypeFile       = "File"
+	storageTypeMemory     = "Memory"
+	eventMeshSecret       = "eventMeshSecret"
+	someSecret            = "namespace/name"
+	publisher             = "publisher"
+	replicas              = "replicas"
+	max                   = "max"
+	min                   = "min"
+	resources             = "resources"
+	logging               = "logging"
+	logLevelInfo          = "Info"
+	logLevelWarn          = "Warn"
+	logLevelError         = "Error"
+	logLevelDebug         = "Debug"
+)
+
+var testEnvironment *integration.TestEnvironment
+
+// TestMain pre-hook and post-hook to run before and after all tests.
+func TestMain(m *testing.M) {
+	// Note: The setup will provision a single K8s env and
+	// all the tests need to create and use a separate namespace
+
+	// setup env test
+	var err error
+	testEnvironment, err = integration.NewTestEnvironment(projectRootDir, true)
+	if err != nil {
+		panic(err)
+	}
+
+	// run tests
+	code := m.Run()
+
+	// tear down test env
+	if err = testEnvironment.TearDown(); err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
+
+func Test_Validate_CreateEventing(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name                      string
+		givenUnstructuredEventing unstructured.Unstructured
+		wantErrMsg                string
+	}{
+		{
+			name: `validation of spec.publisher.replicas.min fails for values > max`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						publisher: map[string]any{
+							replicas: map[string]any{
+								min: 3,
+								max: 2,
+							},
+						},
+					},
+				},
+			},
+			wantErrMsg: "min value must be smaller than the max value",
+		},
+		{
+			name: `validation of spec.publisher.replicas.min passes for values <= max`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						publisher: map[string]any{
+							replicas: map[string]any{
+								min: 2,
+								max: 2,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: `validation of spec.backends.config.eventMeshSecret fails when empty if spec.type = EventMesh`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeEventMesh,
+							config:      map[string]any{},
+						},
+					},
+				},
+			},
+			wantErrMsg: "secret cannot be empty if EventMesh backend is used",
+		},
+		{
+			name: `validation of spec.backends.config.eventMeshSecret passes when set if spec.type = EventMesh`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeEventMesh,
+							config: map[string]any{
+								eventMeshSecret: someSecret,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: `validation of spec.backends.config.eventMeshSecret passes when empty if spec.type = NATS`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeNats,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: `validation of spec.backends.type fails for values other than NATS or EventMesh`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeGibberish,
+						},
+					},
+				},
+			},
+			wantErrMsg: "storage type can only be set to NATS or EventMesh",
+		},
+		{
+			name: `validation of spec.backends.type passes for value = NATS`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeNats,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: `validation of spec.backends.type passes for value = EventMesh`,
+			givenUnstructuredEventing: unstructured.Unstructured{
+				Object: map[string]any{
+					kind:       kindEventing,
+					apiVersion: apiVersionEventing,
+					metadata: map[string]any{
+						name:      testutils.GetRandK8sName(7),
+						namespace: testutils.GetRandK8sName(7),
+					},
+					spec: map[string]any{
+						backends: map[string]any{
+							backendType: typeEventMesh,
+							config: map[string]any{
+								eventMeshSecret: someSecret,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// given
+			testEnvironment.EnsureNamespaceCreation(t, tc.givenUnstructuredEventing.GetNamespace())
+
+			// when
+			err := testEnvironment.CreateUnstructuredK8sResource(&tc.givenUnstructuredEventing)
+
+			// then
+			if tc.wantErrMsg == noError {
+				require.NoError(t, err, "Expected error message to be empty but got error instead."+
+					" Check the validation rule of the eventing CR where the above error is returned.")
+			} else {
+				if err != nil {
+					require.Contains(t, err.Error(), tc.wantErrMsg, "Expected a specific error message."+
+						" but message does not match. Check the validation rules of the eventing CR.")
+				} else {
+					require.Error(t, err, "Expected the following error message: \""+tc.wantErrMsg+
+						".\" but got no error. Check the validation rules of the eventing CR.")
+				}
+
+			}
+		})
+	}
+}
