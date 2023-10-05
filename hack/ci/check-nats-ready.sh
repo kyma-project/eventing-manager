986daa7d46a5aab95d9261566fcb21a941f26c78
Friedrich Wilken <jens.wilken@sap.com>
2023-10-05 17:31:07 +0200

    adjust script

--- hack/ci/check-nats-ready.sh
+++ hack/ci/check-nats-ready.sh
@@ -3,7 +3,7 @@
 timeouttime=${1:-180}
 
 check-nats-ready() {
-  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n kyma-system nats -ojsonpath='{.items[0].status.state}')" != "Processing" ]]; do sleep 1; done'
+  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n kyma-system nats -ojsonpath='{.items[0].status.state}')" != "Ready" ]]; do sleep 1; done'
 
   if [[ $? -ne 0 ]]; then
     echo "NATS was not 'Ready' after ${timeouttime} seconds"
