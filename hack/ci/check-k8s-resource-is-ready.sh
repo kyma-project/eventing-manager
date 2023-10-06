4cf6f4b3b2ee1acbd1455825ca6700bbb67f40fe
Friedrich Wilken <jens.wilken@sap.com>
2023-10-06 10:01:15 +0200

    export variables

--- hack/ci/check-k8s-resource-is-ready.sh
+++ hack/ci/check-k8s-resource-is-ready.sh
@@ -7,7 +7,9 @@ namespace=${3:-kyma-system}
 check-nats-ready() {
   echo "checking ${resource} in the namespace ${namespace} to become 'Ready' for ${timeouttime} seconds"
 
-  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n ${namespace} ${resource} -ojsonpath='{.items[0].status.state}')" != "Ready" ]]; do sleep 1; done'
+  export resource
+  export namespace
+  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n $namespace $resource -ojsonpath='{.items[0].status.state}')" != "Ready" ]]; do sleep 1; done'
 
   if [[ $? -ne 0 ]]; then
     echo "NATS was not 'Ready' after ${timeouttime} seconds"
