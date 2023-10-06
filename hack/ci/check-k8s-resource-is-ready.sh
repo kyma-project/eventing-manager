ed91fea62e3db90f4d27787cb48d38ae8c58f18e
Friedrich Wilken <jens.wilken@sap.com>
2023-10-06 09:53:04 +0200

    improve logging for wait func

--- hack/ci/check-k8s-resource-is-ready.sh
+++ hack/ci/check-k8s-resource-is-ready.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 timeouttime=${1:-180}
+resource=${2:-nats}
+namespace=${3:-kyma-system}
 
 check-nats-ready() {
-  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n kyma-system nats -ojsonpath='{.items[0].status.state}')" != "Ready" ]]; do sleep 1; done'
+  echo "checking ${resource} in the namespace ${namespace} to become 'Ready' for ${timeouttime} seconds"
+
+  timeout ${timeouttime}  bash -c 'while [[ "$(kubectl get -n ${namespace} ${resource} -ojsonpath='{.items[0].status.state}')" != "Ready" ]]; do sleep 1; done'
 
   if [[ $? -ne 0 ]]; then
     echo "NATS was not 'Ready' after ${timeouttime} seconds"
