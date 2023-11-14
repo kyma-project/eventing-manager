937f4e730d90926ca13be91adc97dd4d5a23c974
marcobebway <marco.bebway@sap.com>
2023-11-14 14:41:49 +0100

    Increase object pkg test coverage

--- pkg/object/object.go
+++ pkg/object/object.go
@@ -1,6 +1,8 @@
 package object
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	apigatewayv1beta1 "github.com/kyma-incubator/api-gateway/api/v1beta1"
+)
 
 // Option is a functional option for API objects builders.
-type Option func(metav1.Object)
+type Option func(*apigatewayv1beta1.APIRule)
