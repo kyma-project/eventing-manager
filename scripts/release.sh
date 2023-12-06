b87d849678d11fa9fb55a5fe5a0c90d9ef0542b2
Friedrich Wilken <jens.wilken@sap.com>
2023-12-06 12:09:50 +0100

    Use Github_token

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ set -o pipefail # prevents errors in a pipeline from being masked
 
 # Expected variables:
 #   PULL_BASE_REF - name of the tag
-#   BOT_GITHUB_TOKEN - github token used to upload the template yaml
+#  GITHUB_TOKEN - github token used to upload the template yaml
 
 uploadFile() {
 	filePath=${1}
@@ -16,7 +16,7 @@ uploadFile() {
 
 	response=$(curl -s -o output.txt -w "%{http_code}" \
 		--request POST --data-binary @"$filePath" \
-		-H "Authorization: token $BOT_GITHUB_TOKEN" \
+		-H "Authorization: token $GITHUB_TOKEN" \
 		-H "Content-Type: text/yaml" \
 		$ghAsset)
 	if [[ "$response" != "201" ]]; then
@@ -46,7 +46,7 @@ echo "Updating github release with eventing-manager.yaml"
 echo "Finding release id for: ${PULL_BASE_REF}"
 CURL_RESPONSE=$(curl -w "%{http_code}" -sL \
 	-H "Accept: application/vnd.github+json" \
-	-H "Authorization: Bearer $BOT_GITHUB_TOKEN" \
+	-H "Authorization: Bearer $GITHUB_TOKEN" \
 	https://api.github.com/repos/kyma-project/eventing-manager/releases)
 JSON_RESPONSE=$(sed '$ d' <<<"${CURL_RESPONSE}")
 HTTP_CODE=$(tail -n1 <<<"${CURL_RESPONSE}")
