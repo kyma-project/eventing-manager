dc08d10a355b6553a6f05de26d8f6d04a53d8504
Kiss, Tibor <tibor.kiss01@sap.com>
2023-12-07 15:34:27 +0100

    Sec config

--- scripts/check_image.sh
+++ scripts/check_image.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 REF_NAME="${1:-"main"}"
-
-#SHORT_EXPECTED_SHA=$(git rev-parse --short=8 "${REF_NAME}~")
-#DATE="v$(git show ${SHORT_EXPECTED_SHA} --date=format:'%Y%m%d' --format=%ad -q)"
-
-EXPECTED_TAG="${REF_NAME}"
+SHORT_EXPECTED_SHA=$(git rev-parse --short=8 "${REF_NAME}~")
+DATE="v$(git show ${SHORT_EXPECTED_SHA} --date=format:'%Y%m%d' --format=%ad -q)"
+EXPECTED_TAG="${DATE}-${SHORT_EXPECTED_SHA}"
 
 IMAGE_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/eventing-manager}"
 BUMPED_IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${IMAGE_TO_CHECK}" | cut -d : -f 2)
