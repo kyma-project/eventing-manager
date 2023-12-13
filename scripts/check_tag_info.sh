e526c17b5bc3d449bfc354696b213dd947ce681a
Kiss, Tibor <tibor.kiss01@sap.com>
2023-12-13 15:11:32 +0100

    Collect latest run related data

--- scripts/check_tag_info.sh
+++ scripts/check_tag_info.sh
@@ -11,21 +11,18 @@ DESIRED_TAG="${1:-"main"}"
 
 # Get eventing-manager tag from sec-scanners-config.yaml
 SEC_SCAN_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/eventing-manager}"
+IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${SEC_SCAN_TO_CHECK}" | cut -d : -f 2)
+
 # Get rc-tag
 RC_TAG_TO_CHECK="${3:-rc-tag}"
-
-IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${SEC_SCAN_TO_CHECK}" | cut -d : -f 2)
 RC_TAG=$(cat sec-scanners-config.yaml | grep "${RC_TAG_TO_CHECK}" | cut -d : -f 2)
-
-echo $RC_TAG
-echo $IMAGE_TAG
 
 # Check IMAGE_TAG and required image tag
 if [[ "$IMAGE_TAG" != "$DESIRED_TAG" ]] || [[ "$RC_TAG" != "$DESIRED_TAG" ]]; then
   # ERROR: Tag issue
   echo "Tags are not correct:
   - wanted $DESIRED_TAG
-  - securoty-scanner image tag: $IMAGE_TAG
+  - security-scanner image tag: $IMAGE_TAG
   - rc-tag: $RC_TAG"
   exit 1
 fi
--- scripts/verify-status.sh
+++ scripts/verify-status.sh
@@ -3,7 +3,7 @@
 echo "Checking status of POST Jobs for Eventing-Manager"
 
 REF_NAME="${1:-"main"}"
-TIMEOUT_TIME="${2:-240}"
+TIMEOUT_TIME="${2:-600}"
 INTERVAL_TIME="${3:-3}"
 
 # Generate job Status URL
@@ -18,14 +18,31 @@ function retry {
 
 	# Get status result
 	local statusresult=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" ${STATUS_URL})
+
 	# Get overall state
 	fullstatus=$(echo $statusresult | jq '.state' | tr -d '"')
-	local tudayprowrunstate=$(echo $statusresult | jq --arg t $TODAY_DATE '.statuses[]|select(.created_at | contains($t))' | jq '.state' | tr -d '"')
-	echo $statusresult | jq --arg t $TODAY_DATE '.statuses[]|select(.created_at | contains($t))' | jq '[.]'
 
+	# Collect latest run related data
+	local latestrun=$(echo $statusresult | jq '.statuses[-1]')
+	local latestrun_state=$(echo $latestrun | jq '.state' | tr -d '"')
+	local latestrun_createdat=$(echo $latestrun | jq '.created_at' | tr -d '"')
+	local latestrun_targeturl=$(echo $latestrun | jq '.target_url' | tr -d '"')
+
+	# Check Today's run data
+	if [[ $latestrun_createdat == *"$TODAY_DATE"* ]]; then
+		echo $latestrun_createdat
+		echo $latestrun_state
+		echo $latestrun_targeturl
+	fi
+
+	# Show all execution for Today
+	echo $statusresult | jq --arg t $TODAY_DATE '.statuses[]|select(.created_at | contains($t))'
+
+	# Date time for time-out
 	local CURRENT_TIME=$(date +%s)
 	local elapsed_time=$((CURRENT_TIME - START_TIME))
 
+	# Check time-out
 	if [ $elapsed_time -ge $TIMEOUT_TIME ]; then
 		echo "Timeout reached. Exiting."
 		exit 1
@@ -43,14 +60,13 @@ function retry {
 		sleep $INTERVAL_TIME
 	else
 		echo "Invalid result: $result"
-
 		exit 1
 	fi
 
 }
 
 # Initial wait
-sleep 10
+sleep 0
 # Call retry function
 retry
 while [ "$fullstatus" == "pending" ]; do
