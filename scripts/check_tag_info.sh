4e432099a001ed654cdb4b28f8bbfff34b4873bf
Kiss, Tibor <tibor.kiss01@sap.com>
2023-12-13 16:34:38 +0100

    Remove whitespace

--- scripts/check_tag_info.sh
+++ scripts/check_tag_info.sh
@@ -15,15 +15,15 @@ IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${SEC_SCAN_TO_CHECK}" | cut -d 
 
 # Get rc-tag
 RC_TAG_TO_CHECK="${3:-rc-tag}"
-RC_TAG=$(cat sec-scanners-config.yaml | grep "${RC_TAG_TO_CHECK}" | cut -d : -f 2)
+RC_TAG=$(cat sec-scanners-config.yaml | grep "${RC_TAG_TO_CHECK}" | cut -d : -f 2 | xargs)
 
 # Check IMAGE_TAG and required image tag
 if [[ "$IMAGE_TAG" != "$DESIRED_TAG" ]] || [[ "$RC_TAG" != "$DESIRED_TAG" ]]; then
   # ERROR: Tag issue
   echo "Tags are not correct:
-  - wanted $DESIRED_TAG
-  - security-scanner image tag: $IMAGE_TAG
-  - rc-tag: $RC_TAG"
+  - wanted '$DESIRED_TAG'
+  - security-scanner image tag: '$IMAGE_TAG'
+  - rc-tag: '$RC_TAG'"
   exit 1
 fi
 
