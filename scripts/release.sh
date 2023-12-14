812b4cea3c7d0caa58ef6fcc86bced382fab1ca6
Friedrich Wilken <jens.wilken@sap.com>
2023-12-14 15:31:39 +0100

    format release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,23 +11,23 @@ set -o pipefail # prevents errors in a pipeline from being masked
 #   BOT_GITHUB_TOKEN - github token used to upload the template yaml
 
 uploadFile() {
-  filePath=${1}
-  ghAsset=${2}
+	filePath=${1}
+	ghAsset=${2}
 
-  echo "Uploading ${filePath} as ${ghAsset}"
-  response=$(curl -s -o output.txt -w "%{http_code}" \
-                  --request POST --data-binary @"$filePath" \
-                  -H "Authorization: token $BOT_GITHUB_TOKEN" \
-                  -H "Content-Type: text/yaml" \
-                  $ghAsset)
-  if [[ "$response" != "201" ]]; then
-    echo "Unable to upload the asset ($filePath): "
-    echo "HTTP Status: $response"
-    cat output.txt
-    exit 1
-  else
-    echo "$filePath uploaded"
-  fi
+	echo "Uploading ${filePath} as ${ghAsset}"
+	response=$(curl -s -o output.txt -w "%{http_code}" \
+		--request POST --data-binary @"$filePath" \
+		-H "Authorization: token $BOT_GITHUB_TOKEN" \
+		-H "Content-Type: text/yaml" \
+		$ghAsset)
+	if [[ "$response" != "201" ]]; then
+		echo "Unable to upload the asset ($filePath): "
+		echo "HTTP Status: $response"
+		cat output.txt
+		exit 1
+	else
+		echo "$filePath uploaded"
+	fi
 }
 
 echo "PULL_BASE_REF ${PULL_BASE_REF}"
@@ -39,29 +39,25 @@ cat eventing-manager.yaml
 
 MODULE_VERSION=${PULL_BASE_REF} make module-build
 
-echo "Generated moduletemplate.yaml:"
-cat module-template.yaml
-
 echo "Updating github release with eventing-manager.yaml"
 
 echo "Finding release id for: ${PULL_BASE_REF}"
 CURL_RESPONSE=$(curl -w "%{http_code}" -sL \
-                -H "Accept: application/vnd.github+json" \
-                -H "Authorization: Bearer $BOT_GITHUB_TOKEN" \
-                https://api.github.com/repos/kyma-project/eventing-manager/releases)
-JSON_RESPONSE=$(sed '$ d' <<< "${CURL_RESPONSE}")
-HTTP_CODE=$(tail -n1 <<< "${CURL_RESPONSE}")
+	-H "Accept: application/vnd.github+json" \
+	-H "Authorization: Bearer $BOT_GITHUB_TOKEN" \
+	https://api.github.com/repos/kyma-project/eventing-manager/releases)
+JSON_RESPONSE=$(sed '$ d' <<<"${CURL_RESPONSE}")
+HTTP_CODE=$(tail -n1 <<<"${CURL_RESPONSE}")
 if [[ "${HTTP_CODE}" != "200" ]]; then
-  echo "${JSON_RESPONSE}" && exit 1
+	echo "${JSON_RESPONSE}" && exit 1
 fi
 
 echo "Finding release id for: ${PULL_BASE_REF}"
 RELEASE_ID=$(jq <<<${JSON_RESPONSE} --arg tag "${PULL_BASE_REF}" '.[] | select(.tag_name == $ARGS.named.tag) | .id')
 
-if [ -z "${RELEASE_ID}" ]
-then
-  echo "No release with tag = ${PULL_BASE_REF}"
-  exit 1
+if [ -z "${RELEASE_ID}" ]; then
+	echo "No release with tag = ${PULL_BASE_REF}"
+	exit 1
 fi
 
 UPLOAD_URL="https://uploads.github.com/repos/kyma-project/eventing-manager/releases/${RELEASE_ID}/assets"
