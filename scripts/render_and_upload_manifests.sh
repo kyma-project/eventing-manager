11ac4ad9b325773099e5f3ddd2262e5f1746632f
Friedrich <jens.wilken@sap.com>
2023-12-15 15:53:26 +0100

    remove call of make module-build (#347)
    * remove call of make module-build
    
    We really only need to render the manifests so lets remove module-build.
    
    * revert removal of MODULE_REGISTERY

--- scripts/render_and_upload_manifests.sh
+++ scripts/render_and_upload_manifests.sh
@@ -37,8 +37,7 @@ MODULE_VERSION=${PULL_BASE_REF} make render-manifest
 echo "Generated eventing-manager.yaml:"
 cat eventing-manager.yaml
 
-MODULE_VERSION=${PULL_BASE_REF} make module-build
-
+# MODULE_VERSION=${PULL_BASE_REF} make module-build
 # TODO completly remove the rendering of the module-template from the repository.
 # echo "Generated moduletemplate.yaml:"
 # cat module-template.yaml
