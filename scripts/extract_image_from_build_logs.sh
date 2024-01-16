c2623e2e4900c373ba5110f2bc40fa40962af57a
Muhammad Faizan <m.faizan@sap.com>
2024-01-16 11:38:59 +0100

    updated

--- scripts/extract_image_from_build_logs.sh
+++ scripts/extract_image_from_build_logs.sh
@@ -31,12 +31,10 @@ curl -s -L -o prow-html.html $(echo ${COMMIT_STATUS_JSON} | jq -r '.target_url')
 ## define the log file url.
 LOGS_FILE_URL="$(cat prow-html.html | grep ">Artifacts</a>" | awk -F'"' '{print $2}')/build-log.txt"
 
-### Download the build logs.
+### Download the build logs and extract the image name from build logs.
 echo "Downloading build logs from: ${LOGS_FILE_URL}"
 curl -s -L -o build-log.txt  ${LOGS_FILE_URL}
-
-## extract the image name from build logs.
-export IMAGE_NAME=$(cat build-log1.txt | grep "Successfully built image:" | awk -F " " '{print $NF}')
+export IMAGE_NAME=$(cat build-log.txt | grep "Successfully built image:" | awk -F " " '{print $NF}')
 
 ## validate if image exists
 echo "Validate that image: ${IMAGE_NAME} exists.."
