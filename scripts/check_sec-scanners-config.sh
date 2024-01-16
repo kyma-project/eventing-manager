e19a83edfc6e120d472fcc473aa00cc9f1e8d648
Friedrich <jens.wilken@sap.com>
2024-01-16 09:47:07 +0100

    Release without version input (#396)
    * Fix release of manifests (#340) (#341)
    
    * clean up create-release.yml
    
    Clean up the create-release.yml by removing a number of unused and outcommented lines.
    
    * rename release.sh to render_and_upload_manifests.sh
    
    Rename the scripts/release.sh to render_and_upload_manifests.sh to point clearly out, what the script is about.
    
    * call manifest release from create-release.yml
    
    Call the render_and_upload_manifests.sh because it no longer created by a prow job.
    
    * deactivate module-template upload
    
    Deactivate the upload of the module-template. All functionality for the module-template will be removed eventually, in a follow up PR.
    
    * fix reference of env var
    
    * fix reference to release name (#342)
    
    * add missing env vars for IMG and MODULE_REGISTERY (#343)
    
    * add missing env vars for IMG and MODULE_REGISTERY
    
    * remove arg
    
    * add KUSTOMIZE_VERSION
    
    * fix wrong token (#344)
    
    * add missing env vars for IMG and MODULE_REGISTERY
    
    * remove arg
    
    * replace the BOT_GITHUB_TOKEN with the GITHUB_TOKEN
    
    * remove call of make module-build (#347)
    
    * remove call of make module-build
    
    We really only need to render the manifests so lets remove module-build.
    
    * revert removal of MODULE_REGISTERY
    
    * fix spelling of env var name (#348)
    
    * release-without-version-input
    
    * improve check_sec-scanners-config.sh
    
    rename from check_tag_info.sh to check_sec-scanners-config.sh and add error handling and desciption.
    
    * clean up

--- scripts/check_sec-scanners-config.sh
+++ scripts/check_sec-scanners-config.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-##############################
-# Check tags in security-scan-config.yaml
-# Image Tag, rc-tag
-##############################
+# This script checks thate the RC-Tag and the eventing-manager image have the tag of the corresponding release.
 
+# Error handling:
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
 
 # Get release version
 DESIRED_TAG="${1:-"main"}"
@@ -19,12 +21,12 @@ RC_TAG=$(cat sec-scanners-config.yaml | grep "${RC_TAG_TO_CHECK}" | cut -d : -f 
 
 # Check IMAGE_TAG and required image tag
 if [[ "$IMAGE_TAG" != "$DESIRED_TAG" ]] || [[ "$RC_TAG" != "$DESIRED_TAG" ]]; then
-  # ERROR: Tag issue
-  echo "Tags are not correct:
+	# ERROR: Tag issue
+	echo "Tags are not correct:
   - wanted: $DESIRED_TAG
   - security-scanner image tag: $IMAGE_TAG
   - rc-tag: $RC_TAG"
-  exit 1
+	exit 1
 fi
 
 # OK: Everything is fine
