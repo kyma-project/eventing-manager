dd05d72ba4bdcf489cc8075bbe99cfc581f1474b
Marco Bebway <marco.bebway@sap.com>
2024-03-14 10:09:46 +0100

    chore: remove tests for subscription v1alpha1 (#530)

--- hack/e2e/common/eventing/utils.go
+++ hack/e2e/common/eventing/utils.go
@@ -36,16 +36,6 @@ func CloudEventData(source, eventType string, encoding binding.Encoding) map[str
 	return map[string]interface{}{keyApp: source, keyMode: CloudEventMode(encoding), keyType: eventType}
 }
 
-func ExtractSourceFromSubscriptionV1Alpha1Type(eventType string) string {
-	segments := strings.Split(eventType, ".")
-	return segments[3]
-}
-
-func ExtractLegacyTypeFromSubscriptionV1Alpha1Type(eventTypePrefix, eventSource, eventVersion, eventType string) string {
-	tmp := strings.TrimPrefix(eventType, fmt.Sprintf("%s.%s.", eventTypePrefix, eventSource))
-	return strings.TrimSuffix(tmp, fmt.Sprintf(".%s", eventVersion))
-}
-
 func ExtractLegacyTypeFromSubscriptionV1Alpha2Type(eventVersion, eventType string) string {
 	return strings.TrimSuffix(eventType, fmt.Sprintf(".%s", eventVersion))
 }
@@ -53,22 +43,6 @@ func ExtractLegacyTypeFromSubscriptionV1Alpha2Type(eventVersion, eventType strin
 func ExtractVersionFromEventType(eventType string) string {
 	segments := strings.Split(eventType, ".")
 	return segments[len(segments)-1]
-}
-
-func NewLegacyEventForV1Alpha1(eventType, eventTypePrefix string) (string, string, string, string) {
-	// If the eventType is sap.kyma.custom.noapp.order.created.v1, then for legacy event:
-	// eventSource should be: noapp
-	// eventType should be: order.created
-	// eventVersion should be: v1
-	eventID := uuid.New().String()
-	eventSource := ExtractSourceFromSubscriptionV1Alpha1Type(eventType)
-	eventVersion := ExtractVersionFromEventType(eventType)
-	legacyEventType := ExtractLegacyTypeFromSubscriptionV1Alpha1Type(eventTypePrefix,
-		eventSource, eventVersion, eventType)
-	eventData := LegacyEventData(eventSource, legacyEventType)
-	payload := LegacyEventPayload(eventID, eventVersion, legacyEventType, eventData)
-
-	return eventID, eventSource, legacyEventType, payload
 }
 
 func NewLegacyEvent(eventSource, eventType string) (string, string, string, string) {
