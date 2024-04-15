a28c332202fda43d06a38da10bfcdab8989e1ab9
marcobebway <marco.bebway@sap.com>
2024-04-15 16:05:27 +0200

    Add subscription validation tests

--- internal/controller/eventing/subscription/validator/subscription_test.go
+++ internal/controller/eventing/subscription/validator/subscription_test.go
@@ -1,1 +1,83 @@
 package validator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	eventingv1alpha2 "github.com/kyma-project/eventing-manager/api/eventing/v1alpha2"
+	eventingtesting "github.com/kyma-project/eventing-manager/testing"
+)
+
+func TestValidate(t *testing.T) {
+	ctx := context.TODO()
+
+	const (
+		subName             = "sub"
+		subNamespace        = "test"
+		maxInFlightMessages = "10"
+		sink                = "https://eventing-nats.test.svc.cluster.local:8080"
+	)
+
+	happySinkValidator := SinkValidatorFunc(func(ctx context.Context, sinkURI string) error { return nil })
+	unhappySinkValidator := SinkValidatorFunc(func(ctx context.Context, sinkURI string) error { return ErrSinkValidationFailed })
+
+	tests := []struct {
+		name               string
+		givenSubscription  *eventingv1alpha2.Subscription
+		givenSinkValidator SinkValidator
+		wantErr            error
+	}{
+		{
+			name: "simulate no validation errors",
+			givenSubscription: eventingtesting.NewSubscription(
+				subName,
+				subNamespace,
+				eventingtesting.WithTypeMatchingExact(),
+				eventingtesting.WithEventType(eventingtesting.OrderCreatedV1Event),
+				eventingtesting.WithMaxInFlightMessages(maxInFlightMessages),
+				eventingtesting.WithSink(sink),
+			),
+			givenSinkValidator: happySinkValidator,
+			wantErr:            nil,
+		},
+		{
+			name: "simulate spec validation error",
+			givenSubscription: eventingtesting.NewSubscription(
+				subName,
+				subNamespace,
+				eventingtesting.WithTypeMatchingExact(),
+				eventingtesting.WithTypes([]string{}), // Trigger validation error.
+				eventingtesting.WithMaxInFlightMessages(maxInFlightMessages),
+				eventingtesting.WithSink(sink),
+			),
+			givenSinkValidator: happySinkValidator,
+			wantErr:            ErrSubscriptionValidationFailed,
+		},
+		{
+			name: "simulate sink validation error",
+			givenSubscription: eventingtesting.NewSubscription(
+				subName,
+				subNamespace,
+				eventingtesting.WithTypeMatchingExact(),
+				eventingtesting.WithEventType(eventingtesting.OrderCreatedV1Event),
+				eventingtesting.WithMaxInFlightMessages(maxInFlightMessages),
+				eventingtesting.WithSink(sink),
+			),
+			givenSinkValidator: unhappySinkValidator, // Trigger sink validation error.
+			wantErr:            ErrSinkValidationFailed,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := &subscriptionValidator{sinkValidator: tt.givenSinkValidator}
+			gotErr := validator.Validate(ctx, *tt.givenSubscription)
+			if tt.wantErr == nil {
+				require.NoError(t, gotErr)
+			} else {
+				require.ErrorAs(t, gotErr, &tt.wantErr)
+			}
+		})
+	}
+}
