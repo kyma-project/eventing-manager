e79cf97e58041ea94cade75b85bcf56e9041ebee
Muhammad Faizan <m.faizan@sap.com>
2025-04-23 14:28:05 +0200

    fixed waiting for eventing in k8s compatiblity job

--- hack/e2e/scripts/expose_epp_and_sink.sh
+++ hack/e2e/scripts/expose_epp_and_sink.sh
@@ -66,12 +66,54 @@ spec:
 EOF
 }
 
+wait_for_endpoint_status() {
+  local endpoint="$1"
+  local expected_status="$2"
+  local timeout=300  # Timeout in seconds
+  local interval=5   # Interval between checks in seconds
+  local elapsed=0
+
+  echo "Waiting for endpoint $endpoint to return status $expected_status..."
+
+  while true; do
+    # Get the HTTP status code or capture curl errors
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$endpoint" 2>&1)
+    status_code=$(echo "$response" | grep -Eo '^[0-9]{3}')
+
+    if [ "$status_code" -eq "$expected_status" ]; then
+      echo "Endpoint $endpoint is now returning status $expected_status."
+      return 0
+    fi
+
+    # Check for specific curl errors and retry
+    if echo "$response" | grep -qE "(dial tcp|i/o timeout|no such host)"; then
+      echo "Retrying due to error: $response"
+    else
+      echo "Unexpected response: $response"
+    fi
+
+    # Check if timeout is reached
+    if [ "$elapsed" -ge "$timeout" ]; then
+      echo "Timeout reached while waiting for $endpoint to return status $expected_status."
+      return 1
+    fi
+
+    # Wait and increment elapsed time
+    sleep "$interval"
+    elapsed=$((elapsed + interval))
+  done
+}
+
 wait_for_api_rules_readiness() {
   echo "waiting for EPP APIRule to be ready..."
   kubectl wait apirules.gateway.kyma-project.io -n default epp-e2e-tests --timeout=150s --for=jsonpath='{.status.state}'=Ready
 
   echo "waiting for sink APIRule to be ready..."
   kubectl wait apirules.gateway.kyma-project.io -n default sink-e2e-tests --timeout=150s --for=jsonpath='{.status.state}'=Ready
+
+  echo "wait for endpoints to be reachable"
+  wait_for_endpoint_status "https://sink-e2e-tests.$DOMAIN_TO_EXPOSE_WORKLOADS/events/190d697d-e842-4b0e-bc57-e7731dba6031/" 404
+  wait_for_endpoint_status "https://epp-e2e-tests.$DOMAIN_TO_EXPOSE_WORKLOADS/events" 404
 }
 
 ## Main logic
