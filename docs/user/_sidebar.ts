2fcf976e692a3ac062aa4e3fff0170907e3d1abd
Małgorzata Świeca <malgorzata.swieca@sap.com>
2025-11-13 11:40:47 +0100

    docs: fix links in sidebar (#859)
    * docs: fix links in sidebar
    
    * Delete docs/user/_sidebar.md

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,22 +1,17 @@
 export default [
-  { text: 'Module Lifecycle', link: './01-manager' },
-  { text: 'Configuration', link: './02-configuration' },
-  { text: 'Eventing Architecture', link: './evnt-architecture' },
-  { text: 'Event Names', link: './evnt-event-names' },
-  { text: 'Eventing Metrics', link: './evnt-eventing-metrics' },
-  { text: 'Tutorials', link: './tutorials/evnt-01-prerequisites' },
-  { text: 'Create Subscription Subscribing to Multiple Event Types', link: './tutorials/evnt-02-subs-with-multiple-filters' },
-  { text: 'Event Name Cleanup in Subscriptions', link: './tutorials/evnt-03-type-cleanup' },
-  { text: 'Changing Events Max-In-Flight in Subscriptions', link: './tutorials/evnt-04-change-max-in-flight-in-sub' },
-  { text: 'Publish Legacy Events Using Kyma Eventing', link: './tutorials/evnt-05-send-legacy-events' },
-  { text: 'Resources', link: './resources/README', collapsed: true, items: [
-    { text: 'Subscription CR', link: './resources/evnt-cr-subscription' }
+  { text: 'Event Naming and Cleanup', link: './evnt-event-names.md' },
+  { text: 'Eventing Metrics', link: './evnt-eventing-metrics.md' },
+  { text: 'Choose an Eventing Backend', link: './evnt-01-choose-backend.md' },
+  { text: 'Configure SAP Event Mesh for Kyma Eventing', link: './tutorials/evnt-01-configure-event-mesh.md' },
+  { text: 'Subscribe to Multiple Event Types', link: './tutorials/evnt-02-subs-with-multiple-filters.md' },
+  { text: 'Manage Subscriber Workload with Max-In-Flight', link: './tutorials/evnt-04-change-max-in-flight-in-sub.md' },
+  { text: 'Troubleshooting for the Eventing Module', link: './troubleshooting/README.md', collapsed: true, items: [
+    { text: 'General Diagnostics: Event Not Delivered', link: './troubleshooting/evnt-01-eventing-troubleshooting.md' },
+    { text: 'Subscriber Receives Irrelevant Events', link: './troubleshooting/evnt-02-subscriber-irrelevant-events.md' },
+    { text: 'NATS Backend Storage Is Full', link: './troubleshooting/evnt-03-free-jetstream-storage.md' }
     ] },
-  { text: 'Troubleshooting', link: './troubleshooting/README', collapsed: true, items: [
-    { text: 'Kyma Eventing - Basic Diagnostics', link: './troubleshooting/evnt-01-eventing-troubleshooting' },
-    { text: 'NATS JetStream Backend Troubleshooting', link: './troubleshooting/evnt-02-jetstream-troubleshooting' },
-    { text: 'Subscriber Receives Irrelevant Events', link: './troubleshooting/evnt-03-type-collision' },
-    { text: 'Eventing Backend Stopped Receiving Events Due To Full Storage', link: './troubleshooting/evnt-04-free-jetstream-storage' },
-    { text: 'Published Events Are Pending in the Stream', link: './troubleshooting/evnt-05-fix-pending-messages' }
+  { text: 'Resources', link: './resources/README.md', collapsed: true, items: [
+    { text: 'Eventing CR', link: './resources/eventing-cr.md' },
+    { text: 'Subscription CR', link: './resources/subscription-cr.md' }
     ] }
 ];
