07a78869bc9192b4e1ebee1d34c1389f5f47ed3b
Małgorzata Świeca <malgorzata.swieca@sap.com>
2025-11-14 13:10:08 +0100

    docs: update CODEOWNERS and paths in the sidebar

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,17 +1,17 @@
 export default [
-  { text: 'Event Naming and Cleanup', link: './evnt-event-names.md' },
-  { text: 'Eventing Metrics', link: './evnt-eventing-metrics.md' },
-  { text: 'Choose an Eventing Backend', link: './evnt-01-choose-backend.md' },
-  { text: 'Configure SAP Event Mesh for Kyma Eventing', link: './tutorials/evnt-01-configure-event-mesh.md' },
-  { text: 'Subscribe to Multiple Event Types', link: './tutorials/evnt-02-subs-with-multiple-filters.md' },
-  { text: 'Manage Subscriber Workload with Max-In-Flight', link: './tutorials/evnt-04-change-max-in-flight-in-sub.md' },
-  { text: 'Troubleshooting for the Eventing Module', link: './troubleshooting/README.md', collapsed: true, items: [
-    { text: 'General Diagnostics: Event Not Delivered', link: './troubleshooting/evnt-01-eventing-troubleshooting.md' },
-    { text: 'Subscriber Receives Irrelevant Events', link: './troubleshooting/evnt-02-subscriber-irrelevant-events.md' },
-    { text: 'NATS Backend Storage Is Full', link: './troubleshooting/evnt-03-free-jetstream-storage.md' }
+  { text: 'Event Naming and Cleanup', link: './evnt-event-names' },
+  { text: 'Eventing Metrics', link: './evnt-eventing-metrics' },
+  { text: 'Choose an Eventing Backend', link: './evnt-01-choose-backend' },
+  { text: 'Configure SAP Event Mesh for Kyma Eventing', link: './tutorials/evnt-01-configure-event-mesh' },
+  { text: 'Subscribe to Multiple Event Types', link: './tutorials/evnt-02-subs-with-multiple-filters' },
+  { text: 'Manage Subscriber Workload with Max-In-Flight', link: './tutorials/evnt-04-change-max-in-flight-in-sub' },
+  { text: 'Troubleshooting for the Eventing Module', link: './troubleshooting/README', collapsed: true, items: [
+    { text: 'General Diagnostics: Event Not Delivered', link: './troubleshooting/evnt-01-eventing-troubleshooting' },
+    { text: 'Subscriber Receives Irrelevant Events', link: './troubleshooting/evnt-02-subscriber-irrelevant-events' },
+    { text: 'NATS Backend Storage Is Full', link: './troubleshooting/evnt-03-free-jetstream-storage' }
     ] },
-  { text: 'Resources', link: './resources/README.md', collapsed: true, items: [
-    { text: 'Eventing CR', link: './resources/eventing-cr.md' },
-    { text: 'Subscription CR', link: './resources/subscription-cr.md' }
+  { text: 'Resources', link: './resources/README', collapsed: true, items: [
+    { text: 'Eventing CR', link: './resources/eventing-cr' },
+    { text: 'Subscription CR', link: './resources/subscription-cr' }
     ] }
 ];
