83c3bee42c87a8d3e9a73dfd2bdba21a4282e1fc
Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
2026-07-21 10:00:18 +0200

    Update _sidebar.ts

--- docs/user/_sidebar.ts
+++ docs/user/_sidebar.ts
@@ -1,7 +1,7 @@
 export default [
   { text: 'Event Naming and Cleanup', link: './evnt-event-names' },
   { text: 'Eventing Metrics', link: './evnt-eventing-metrics' },
-  { text: 'Choose an Eventing Backend', link: './evnt-01-choose-backend' },
+  { text: 'Choose an Eventing Backend', link: './tutorials/evnt-01-choose-backend' },
   { text: 'Configure SAP Event Mesh for Kyma Eventing', link: './tutorials/evnt-01-configure-event-mesh' },
   { text: 'Subscribe to Multiple Event Types', link: './tutorials/evnt-02-subs-with-multiple-filters' },
   { text: 'Manage Subscriber Workload with Max-In-Flight', link: './tutorials/evnt-04-change-max-in-flight-in-sub' },
